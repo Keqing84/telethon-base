7fe85a23405fd13e2da43f3fd6092bef748a34ba
Sensei Keqing <k.izuka@yandex.com>
2024-01-16 22:46:40 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,1 +1,17 @@
-# Heelo
+from bot import app
+from telethon import events, Button
+from bot.utils.utils import BtCrt
+
+@app.on(events.NewMessage(incoming=True, pattern="/start"))
+async def _start(event):
+    bt = BtCrt()
+    bt.add_url("Test URl","https://t.mekai8_4")
+    bt.add_callback("Inline Button", "checkss")
+    await event.reply("Hello!, This Is A Test Bot Running In Telethon",
+                    buttons=bt.btns
+             )
+
+
+@app.on(events.callbackquery.CallbackQuery(data="checkss"))
+async def _callby(event):
+    await event.edit("You clicked a button!")
