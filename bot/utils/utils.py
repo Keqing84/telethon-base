0410ebfd85ef212a1fa292544501caa5f8685f79
Sensei Keqing <k.izuka@yandex.com>
2024-01-16 22:26:34 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -1,1 +1,14 @@
-# yet To Upload
+import os
+import importlib
+from pathlib import Path
+
+def load_plugins():
+    plugins_folder = "plugins"
+
+    # Iterate through files in the plugins folder
+    for file_path in Path(plugins_folder).glob("*.py"):
+        # Get the module name from the file name
+        module_name = file_path.stem
+
+        # Import the module dynamically
+        module = importlib.import_module(f"{plugins_folder}.{module_name}")
