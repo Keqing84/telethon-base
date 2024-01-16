cbe0036868f3fba52434403f7c371968adc14a45
Sensei Keqing <k.izuka@yandex.com>
2024-01-16 22:14:02 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,4 +3,4 @@ from bot.config import Config
 from telethon import TelegramClient
 
 
-bot = TelegramClient('anon', Config.api_id, Config.api_hash).start(bot_token=Config.bot_token)
+app = TelegramClient('anon', Config.api_id, Config.api_hash).start(bot_token=Config.bot_token)
