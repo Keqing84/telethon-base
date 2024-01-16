aa3d6c8edcdcafc5f644b87688e9fd189a52129f
Sensei Keqing <k.izuka@yandex.com>
2024-01-16 22:34:48 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -2,6 +2,21 @@ import os
 import importlib
 from pathlib import Path
 
+from telethon import Button
+
+
+class BtCrt:
+    def __init__(self):
+        self.btns = []
+
+    def add_url(self, text, url):
+        self.btns.append([Button.url(text, url=url)])
+
+    def add_callback(self, text, call_data):
+        self.btns.append([Button.inline(text, data=url)])
+
+
+
 def load_plugins():
     plugins_folder = "plugins"
 
