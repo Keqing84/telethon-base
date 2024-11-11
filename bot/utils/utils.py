4eae5f0caa2f7e5c6933561cdeff0826df396b4d
Sensei Keqing <k.izuka@yandex.com>
2024-11-12 03:59:37 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -1,9 +1,9 @@
 import os
+import glob
 import importlib
-from pathlib import Path
 
 from telethon import Button
-
+from bot.logging import LOG
 
 class BtCrt:
     def __init__(self):
@@ -12,18 +12,19 @@ class BtCrt:
     def add_url(self, text, url):
         self.btns.append([Button.url(text, url=url)])
 
-    def add_callback(self, text, call_data):
-        self.btns.append([Button.inline(text, data=url)])
+    def add_callback(self, text, data):
+        self.btns.append([Button.inline(text, data=data)])
 
 
 
 def load_plugins():
-    plugins_folder = "plugins"
-
-    # Iterate through files in the plugins folder
-    for file_path in Path(plugins_folder).glob("*.py"):
-        # Get the module name from the file name
-        module_name = file_path.stem
-
-        # Import the module dynamically
-        module = importlib.import_module(f"{plugins_folder}.{module_name}")
+    for file_path in glob.glob("bot/plugins/**/*.py", recursive=True):
+        module = ".".join(file_path.split("/"))[:-3]
+        module_name = file_path.split("/")[-1][:-3]
+        if module_name in ["__init__"]:
+            continue
+        try:
+            importlib.import_module(module)
+            LOG.info(f"Imported Module: {module_name}.py")
+        except Exception as e:
+            LOG.info(f"Can't Import Module {module_name}.py: {str(e)}")
