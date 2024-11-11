c73ffab3abd03cdabb87edd53b081d43b6bec0d1
Sensei Keqing <k.izuka@yandex.com>
2024-11-12 03:36:06 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -5,8 +5,8 @@ from bot.utils.utils import BtCrt
 @app.on(events.NewMessage(incoming=True, pattern="/start"))
 async def _start(event):
     bt = BtCrt()
-    bt.add_url("Test URl","https://t.mekai8_4")
-    bt.add_callback("Inline Button", "checkss")
+    bt.add_url(text="Test URl",url="https://t.mekai8_4")
+    bt.add_callback(text="Inline Button", data="checkss")
     await event.reply("Hello!, This Is A Test Bot Running In Telethon",
                     buttons=bt.btns
              )
