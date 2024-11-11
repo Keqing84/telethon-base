698c65d4198ac2fc13ffead6577959aa38afa464
Sensei Keqing <k.izuka@yandex.com>
2024-11-12 03:35:19 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,9 @@
 from bot import app
 from bot.utils.utils import load_plugins
-
-load_plugins()
-print("Bot Imported All Plugins")
-print("Bot Started Successfully")
+from bot.logging import LOG
 
 if __name__ == "__main__":
+    load_plugins()
+    LOG.info("Bot Imported All Plugins")
+    LOG.info("Bot Started Successfully")
     app.run_until_disconnected()
